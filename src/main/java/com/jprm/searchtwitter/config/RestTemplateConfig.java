e8caef0ed7cbfffe9aef76d43642163520273bdb
joao.menezes <joaopedro6br@gmail.com>
2019-10-19 14:03:38 -0300

    Configuração Logging

--- src/main/java/com/jprm/searchtwitter/config/RestTemplateConfig.java
+++ src/main/java/com/jprm/searchtwitter/config/RestTemplateConfig.java
@@ -1,15 +1,37 @@
 package com.jprm.searchtwitter.config;
 
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+import org.zalando.logbook.Logbook;
+import org.zalando.logbook.httpclient.LogbookHttpRequestInterceptor;
+import org.zalando.logbook.httpclient.LogbookHttpResponseInterceptor;
 
 @Configuration
 public class RestTemplateConfig {
 
+	@Autowired
+	private Logbook logbook;
+	
 	@Bean
 	public RestTemplate restTemplate() {
-		return new RestTemplate();
+
+		CloseableHttpClient client = HttpClientBuilder.create()
+		        .addInterceptorFirst(new LogbookHttpRequestInterceptor(logbook))
+		        .addInterceptorFirst(new LogbookHttpResponseInterceptor())
+		        .disableContentCompression()
+		        .build();
+		
+		RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory(client));
+		
+		//restTemplate.
+		
+		return restTemplate;
+
 	}
 	
 }
